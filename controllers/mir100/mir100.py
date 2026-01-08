4fc723ed64e8a57bedb79663ae8641dd1bbf08a7
Tom-BnK <nhat.nguyen2@bnksolution.com>
2026-01-08 14:39:22 +0700

    feat: add motor to robot

--- controllers/mir100/mir100.py
+++ controllers/mir100/mir100.py
@@ -19,13 +19,38 @@ robot = Robot()
 time_step = int(robot.getBasicTimeStep())
 
 # ===== Motors =====
-motor_left = robot.getDevice("middle_left_wheel_joint")
-motor_right = robot.getDevice("middle_right_wheel_joint")
+motor_front_left_joint = robot.getDevice("front_left_wheel_joint")
+motor_front_right_joint = robot.getDevice("front_right_wheel_joint")
+motor_back_left_joint = robot.getDevice("back_left_wheel_joint")
+motor_back_right_joint = robot.getDevice("back_right_wheel_joint")
 
-motor_left.setPosition(float("inf"))
-motor_right.setPosition(float("inf"))
-motor_left.setVelocity(0.0)
-motor_right.setVelocity(0.0)
+
+motor_front_left = robot.getDevice("front_left_wheel")
+motor_front_right = robot.getDevice("front_right_wheel")
+motor_back_left = robot.getDevice("back_left_wheel")
+motor_back_right = robot.getDevice("back_right_wheel")
+
+
+motor_front_left_joint.setPosition(float("inf"))
+motor_back_left_joint.setPosition(float("inf"))
+motor_front_right_joint.setPosition(float("inf"))
+motor_back_right_joint.setPosition(float("inf"))
+
+motor_front_left.setPosition(float("inf"))
+motor_back_left.setPosition(float("inf"))
+motor_front_right.setPosition(float("inf"))
+motor_back_right.setPosition(float("inf"))
+
+motor_front_left_joint.setVelocity(0.0)
+motor_back_left_joint.setVelocity(0.0)
+motor_front_right_joint.setVelocity(0.0)
+motor_back_right_joint.setVelocity(0.0)
+
+
+motor_front_left.setVelocity(0.0)
+motor_back_left.setVelocity(0.0)
+motor_front_right.setVelocity(0.0)
+motor_back_right.setVelocity(0.0)
 
 # ===== Sensors =====
 depth_camera = robot.getDevice("depth_camera")
@@ -51,20 +76,16 @@ while robot.step(time_step) != -1:
     is_key_valid = True
 
     if key == Keyboard.UP:
-        target_speed += SPEED_INCREMENT
-        target_speed = min(target_speed, SPEED_MAX)
+        target_speed = 12
 
     elif key == Keyboard.DOWN:
-        target_speed -= SPEED_INCREMENT
-        target_speed = max(target_speed, SPEED_MIN)
+        target_speed = -6
 
     elif key == Keyboard.LEFT:
-        target_omega += ANGULAR_SPEED_INCREMENT
-        target_omega = min(target_omega, ANGULAR_SPEED_MAX)
+        target_omega = 3
 
     elif key == Keyboard.RIGHT:
-        target_omega -= ANGULAR_SPEED_INCREMENT
-        target_omega = max(target_omega, ANGULAR_SPEED_MIN)
+        target_omega = 3
 
     elif key == ord(' '):
         target_speed = 0.0
@@ -77,8 +98,16 @@ while robot.step(time_step) != -1:
         print(f"vx:{target_speed:.2f}[m/s] ω:{target_omega:.2f}[rad/s]")
 
         # ===== Wheel speed computation =====
-        left_speed = (target_speed - target_omega * DISTANCE_TO_CENTER) / WHEEL_RADIUS
-        right_speed = (target_speed + target_omega * DISTANCE_TO_CENTER) / WHEEL_RADIUS
+        # left_speed = (target_speed - target_omega * DISTANCE_TO_CENTER) / WHEEL_RADIUS
+        # right_speed = (target_speed + target_omega * DISTANCE_TO_CENTER) / WHEEL_RADIUS
 
-        motor_left.setVelocity(left_speed)
-        motor_right.setVelocity(right_speed)
+        motor_front_left_joint.setVelocity(target_omega)
+        motor_front_right_joint.setVelocity(target_omega)
+        motor_back_left_joint.setVelocity(target_omega)
+        motor_back_right_joint.setVelocity(target_omega)
+
+        motor_front_left.setVelocity(target_speed)
+        motor_front_right.setVelocity(target_speed)
+        motor_back_left.setVelocity(target_speed)
+        motor_back_right.setVelocity(target_speed)
+
